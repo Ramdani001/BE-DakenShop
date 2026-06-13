64c1e0428518bcd7243fd4c7ebce4642f7238851
Rizkan Ramdani <rizkan0990@gmail.com>
2026-06-13 16:14:06 +0700

    a
    s

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -86,12 +86,12 @@ export class CartService {
         const item = await prisma.cartItem.findUnique({ where: { id: cartItemId } });
         if (!item) throw new Error(`Cart item with ID ${cartItemId} not found.`);
         
-        console.log("ID yang diterima:",item);
+        return item;
 
-        return await prisma.cartItem.update({
-            where: { id: cartItemId },
-            data: { quantity },
-        });
+        // return await prisma.cartItem.update({
+        //     where: { id: cartItemId },
+        //     data: { quantity },
+        // });
     }
 
     async removeItem(cartItemId: string) {
